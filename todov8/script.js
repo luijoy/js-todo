116715153a8bf0bbc12a0c275df2d7182b08517b
Lui Kohl <lui.kohl@gmail.com>
2018-06-29 19:46:40 +0200

    completed v8 with all inputs and buttons

--- todov8/script.js
+++ todov8/script.js
@@ -61,6 +61,28 @@
     displayTodos: function () {
       todoList.displayTodos();
     },
+    addTodo: function () {
+      var addTodoTextInput = document.getElementById('addTodoTextInput');
+      todoList.addTodo(addTodoTextInput.value);
+      addTodoTextInput.value = "";
+    },
+    changeTodo: function () {
+      var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+      var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+      todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value)
+      changeTodoPositionInput.value = "";
+      changeTodoTextInput.value = "";
+    },
+    deleteTodo: function () {
+      var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
+      todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+      deleteTodoPositionInput.value = "";
+    },
+    toggleCompleted: function () {
+      var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
+      todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+      toggleCompletedPositionInput.value = "";
+    },
     toggleAll: function () {
       todoList.toggleAll();
     }
