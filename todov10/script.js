96e3f2f16c911d178ae1ad67acae25608777293f
Lui Kohl <lui.kohl@gmail.com>
2018-07-03 16:36:06 +0200

    completed v10

--- todov10/script.js
+++ todov10/script.js
@@ -54,10 +54,8 @@
       view.displayTodos();
 
     },
-    deleteTodo: function () {
-      var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-      todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-      deleteTodoPositionInput.value = "";
+    deleteTodo: function (position) {
+      todoList.deleteTodo(position);
       view.displayTodos();
 
     },
@@ -90,9 +88,30 @@
         } else {
           todoTextWithCompletion = `( ) ${todo.todoText}`;
         }
-
+        todoLi.id = i;
         todoLi.textContent = todoTextWithCompletion;
+        todoLi.appendChild(this.createDeleteButton());
         todosUl.appendChild(todoLi);
       }
+    },
+    createDeleteButton: function () {
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'deleteButton';
+      return deleteButton;
+    },
+    setUpEventListeners: function () {
+      var todosUl = document.querySelector('ul');
+
+      todosUl.addEventListener('click', function (event) {
+
+        var elementClicked = event.target;
+
+        if (elementClicked.className === 'deleteButton') {
+          handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+        }
+      })
     }
   };
+
+  view.setUpEventListeners();
